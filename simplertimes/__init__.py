d04cada1a27e00b4aeca0e41869ae581f2a5816a
Alvaro <alvaro@alvaronaceda.es>
2020-01-19 18:13:49 +0100

    Changed build directory

--- simplertimes/__init__.py
+++ simplertimes/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config')
+    
+    @app.route('/admin')
+    def admin():
+        return app.config["BANANA"]
 
     @app.route('/')
     def root():
